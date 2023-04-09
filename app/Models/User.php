748ca42c757365b2ce1f1009b73c79212212ce70
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-04-09 17:33:53 +0430

    change email to phone number

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,6 +40,6 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        'phone_number_verified_at' => 'datetime',
     ];
 }
