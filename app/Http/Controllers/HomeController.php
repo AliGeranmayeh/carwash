217663e71e1596b49eb01ba58dd9b84b9fa42771
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-04-10 22:28:59 +0430

    update controller for post data in home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\WashingType;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,54 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('home',[
+            "total_time" => 0,
+            "total_payment" => 0 ,
+            "error"=> ''
+        ]);
     }
+    public function submitForm()
+    {
+        $total_time = 0;
+        $total_payment = 0;
+        $post = $_POST;
+        if (count($post)==1) {
+            return view('home',[
+                "total_time" => 0,
+                "total_payment" => 0 ,
+                "error" => "please click on at least one check box",
+            ]);
+        }
+        else {
+            $washing_type = new WashingType;
+            $washing_type->user_id = Auth::user()->id;
+            $washing_type->car_body = $post['car_body'] ?? 0;
+            $washing_type->zero_washing = $post['zero_washing']?? 0;
+            $washing_type->interior_leaning = $post['interior_leaning'] ?? 0;
+
+            if ($washing_type->zero_washing) {
+                $total_time += 60;
+                $total_payment+=80;
+                $washing_type->zero_washing=1;
+            }
+            if ($washing_type->car_body) {
+                $total_time += 15;
+                $total_payment+=25;
+                $washing_type->car_body = 1;
+            }
+            if ($washing_type->interior_leaning) {
+                $total_time += 20;
+                $total_payment+=30;
+                $washing_type->interior_leaning=1;
+            }
+            $washing_type->time = $total_time;
+            $washing_type->payment = $total_payment;
+            $washing_type->save();
+            
+            return redirect()->route('time_reservation');
+
+        }
+        
+    }
+    
 }
