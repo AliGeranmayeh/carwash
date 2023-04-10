fbe9d9d4146915194bad236cbef9b2582e8f033f
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-04-10 22:54:44 +0430

    add database fields to time reservation table

--- database/migrations/2023_04_10_175401_create_time_reservations_table.php
+++ database/migrations/2023_04_10_175401_create_time_reservations_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('time_reservations', function (Blueprint $table) {
             $table->id();
+            $table->string('date');
+            $table->string('time');
+            $table->string('section');
+            $table->unsignedBigInteger('washing_id')->unique();
+            $table->foreign('washing_id')->references('id')->on('washing_types')->onDelete('cascade');
             $table->timestamps();
         });
     }
