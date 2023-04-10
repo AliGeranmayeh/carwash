892570d953877ff4c049ea9b58a94ed60d23cab8
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-04-10 22:26:59 +0430

    update home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,44 @@
 
 @section('content')
 <div class="container">
+    @if ($error !== "")
+    <div class="alert alert-danger" role="alert">
+        <p>{{$error}}</p>
+      </div>    
+    @endif
     <div class="row justify-content-center">
         <div class="col-md-8">
-            
+            <div class="card">
+                <div class="card-header">{{__(Auth::user()->name)}}</div>
+
+                <div class="card-body">
+                    <form action="" method="post">
+                        @csrf{{ csrf_field() }}
+                        <div class="form-check">
+                            <input class="form-check-input"name=car_body type="checkbox" value="true" id="check">
+                            <label class="form-check-label" for="car_body">
+                              Outside Washing ---> 15 Minutes , 25 Toman
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" name=interior_leaning type="checkbox" value="true" id="check">
+                            <label class="form-check-label" for="interior_leaning">
+                                Interior Cleaning ---> 20 Minutes , 30 Toman
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" name=zero_washing type="checkbox" value="true" id="check">
+                            <label class="form-check-label" for="zero_washing">
+                                Zero Washing ---> 60 Minutes , 80 Toman
+                            </label>
+                        </div>
+                        
+                        <input type="submit" name=submit value="Submit" id="checkBtn">
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 </div>
+
 @endsection
