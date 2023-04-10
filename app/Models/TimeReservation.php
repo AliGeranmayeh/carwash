88b02405455496ec3741d05828bb45713e88f900
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-04-10 22:55:11 +0430

    define relation between time reservation and washing type

--- app/Models/TimeReservation.php
+++ app/Models/TimeReservation.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class TimeReservation extends Model
 {
     use HasFactory;
+
+    public function washing_id(): BelongsTo
+    {
+        return $this->belongsTo(WashingType::class);
+    }
 }
--- app/Models/WashingType.php
+++ app/Models/WashingType.php
@@ -5,25 +5,32 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class WashingType extends Model
 {
     use HasFactory;
-
-    public function user(): BelongsTo
-    {
-        return $this->belongsTo(User::class);
-    }
-
+    protected $attributes = [
+        'state' => 0,
+    ];
     protected $fillable = [
         'user_id',
         'car_body',
         'interior_leaning',
         'zero_washing',
         'payment',
-        'time'
+        'time',
+        'state'
     ];
     protected $hidden = [
         'user_id'
     ];
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function reservation(): HasOne
+    {
+        return $this->hasOne(TimeReservation::class);
+    }
 }
